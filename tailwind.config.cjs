12cf26a3f11843a94060ce0857b12b02ca62cdd6
livelavishlifee-eng <livelavishlifee@gmail.com>
2026-04-30 07:58:07 -1200

    Add files via upload

---
+++ tailwind.config.cjs
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./App.tsx"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))'
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))'
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))'
+        },
+      },
+      fontFamily: {
+        sans: ["'Inter'", "sans-serif"],
+        serif: ["'Lora'", "serif"],
+        mono: ["'Space Mono'", "monospace"],
+      },
+    }
+  },
+  plugins: [require("tailwindcss-animate")],
+};
